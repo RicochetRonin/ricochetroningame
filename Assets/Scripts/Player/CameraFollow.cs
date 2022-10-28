ad59bfac64cb915174722ecf2d4dce58c821c359
Dylan Foster <61020433+Dylan-Foster@users.noreply.github.com>
2022-10-28 17:26:28 -0400

    playtesting

--- Assets/Scripts/Player/CameraFollow.cs
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -6,38 +6,15 @@ public class CameraFollow : MonoBehaviour
 {
     public Transform target;
     public float smoothTime = 0.3F;
-    public GameObject Leftwall;
-    public GameObject RightWall;
-    public Camera SceneCamera;
-    private BoxCollider2D _LeftWallCollider;
-    private BoxCollider2D _RightWallCollider;
-    private float _LeftBound;
-    private float _RightBound;
-    private Vector3 _CurrentPos;
+    public float posY;
     private Vector3 velocity = Vector3.zero;
-    private float _CameraExtent;
 
-    private void Start()
-    {
-        //_LeftWallCollider = Leftwall.GetComponent<BoxCollider2D>();
-        //_RightWallCollider = RightWall.GetComponent<BoxCollider2D>();
-
-        //_LeftBound = _LeftWallCollider.bounds.center.x - _LeftWallCollider.bounds.extents.x;
-        //_RightBound = _RightWallCollider.bounds.center.x + _RightWallCollider.bounds.extents.x;
-
-        _CameraExtent = SceneCamera.GetComponent<Camera>().orthographicSize * SceneCamera.GetComponent<Camera>().aspect;
-    }
-
-    void FixedUpdate()
+    void Update()
     {
         // Define a target position above and behind the target transform
-        Vector3 targetPosition = target.position;
-
-        //targetPosition.x = Mathf.Clamp(target.position.x, _LeftBound + _CameraExtent, _RightBound - _CameraExtent);
-
-        _CurrentPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        Vector3 targetPosition = target.TransformPoint(new Vector3(0, posY, -10));
 
         // Smoothly move the camera towards that target position
-        transform.position = Vector3.SmoothDamp(_CurrentPos, targetPosition, ref velocity, smoothTime);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
     }
 }
