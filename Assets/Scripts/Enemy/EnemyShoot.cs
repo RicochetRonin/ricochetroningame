e40f1787726456339bc5285ee0d9882d5a16fac9
sdembeck0 <sdembeck0@gmail.com>
2022-11-18 21:29:30 -0500

    Moved laser script, added enemy shoot animation, modified code to point enemy sprite in direction of player

--- Assets/Scripts/Enemy/EnemyShoot.cs
+++ Assets/Scripts/Enemy/EnemyShoot.cs
@@ -12,6 +12,7 @@ public class EnemyShoot : MonoBehaviour
     [SerializeField] protected bool canAttack = false;
     [SerializeField] protected float firstShotDelay = 2f;
     [SerializeField] private AudioClip shootSFX;
+    [SerializeField] private Animator animator;
 
     void OnEnable()
     {
@@ -33,6 +34,7 @@ public class EnemyShoot : MonoBehaviour
             //bulletPrefab.GetComponent<BulletController>().SetHostile();
             MasterPool.Spawn(bulletPrefab, transform.position, transform.rotation);
             AudioManager.PlayOneShotSFX(shootSFX);
+            animator.SetTrigger("Shoot");
 
             canAttack = false;
             StartCoroutine("ResetCoolDown");
