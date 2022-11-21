18dfaf28f0fd65a891366e40af21272ca264a90e
Soultides <ngodoug@yahoo.com>
2022-11-20 19:18:48 -0500

    Adding Bullet prefab references

--- Assets/Scripts/Bullets/BulletVFXPool.cs
+++ Assets/Scripts/Bullets/BulletVFXPool.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletVFXPool
+public class BulletVFXPool : MonoBehaviour
 {
     public Stack<GameObject> inactive = new Stack<GameObject>();
     public GameObject parent;
