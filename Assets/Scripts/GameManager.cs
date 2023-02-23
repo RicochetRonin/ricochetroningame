139b6bc65117685ef4c06997c2a2ebffcaac315d
seanpgillick <75447516+seanpgillick@users.noreply.github.com>
2023-02-22 19:39:47 -0500

    StatsUI appears when player runs into scene changer

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -37,7 +37,7 @@ public class GameManager : MonoBehaviour
     public static int bulletsReflected = 0;
     public static int playerDeaths = 0;
     public Time totalTime;
-    private static string timerVal = "";
+    public static string timerVal = "";
 
 
 
